92d86e9e2ba7a2c5ffe3d46f5536367910c21703
DotViegas <160679178+DotViegas@users.noreply.github.com>
2024-05-01 13:40:29 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let tamanhoFoto = 500;
+let seletorImg = document.querySelector("img")
+
+seletorImg.setAttribute("height", tamanhoFoto);
+seletorImg.setAttribute("width", tamanhoFoto);
+
+//function alterarTamanhoFoto(tag, altura, largura) {
+    //let foto = document.querySelector(tag);
+    //foto.setAttribute("height", altura);
+    //foto.setAttribute("width", largura);
+//}
+
+//alterarTamanhoFoto('img', tamanhoFoto, tamanhoFoto); // Selecione a imagem diretamente pelo tag 'img'
+
